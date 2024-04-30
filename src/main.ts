4e5faf3bee22971abf5f60692e1ac69f1a47e574
TLOWAC <leechanghun55@gmail.com>
2024-04-30 19:33:25 +0900

    feat: swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // validation pipe 설정
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,6 +14,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('Colavo API Example')
+    .setDescription('Colavo')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
