5fcbe257b044c05b9d091cece1caf4a43b14bb0b
TLOWAC <leechanghun55@gmail.com>
2024-05-01 12:09:09 +0900

    refactor: swagger 데코레이터 수정

--- src/domain/reservation/dto/timeslot.req-dto.ts
+++ src/domain/reservation/dto/timeslot.req-dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
 import {
   IsString,
   IsNumber,
@@ -8,40 +9,69 @@ import {
 } from 'class-validator';
 
 export class TimeSlotsReqDto {
-  @ApiProperty({ description: '시작일 ( 예 : "YYYYMMDD" )' })
+  @ApiProperty({
+    name: 'start_day_identifier',
+    description: '시작일 ( 예 : "YYYYMMDD" )',
+    default: '20210509',
+  })
+  @Expose({ name: 'start_day_identifier' })
   @IsString()
-  start_day_identifier: string;
+  startDayIdentifier: string;
 
   @ApiProperty({
+    name: 'timezone_identifier',
     description: '타임존 ( 예 : "America/Los_Angeles", "Asia/Seoul" )',
+    default: 'Asia/Seoul',
   })
+  @Expose({ name: 'timezone_identifier' })
   @IsString()
   @IsTimeZone()
-  timezone_identifier: string;
+  timezoneIdentifier: string;
 
-  @ApiProperty({ description: '미용 서비스 제공 시간 ( s, 3600 > 1시간 )' })
+  @ApiProperty({
+    name: 'service_duration',
+    description: '미용 서비스 제공 시간 ( s, 3600 > 1시간 )',
+    default: 3600,
+  })
+  @Expose({ name: 'service_duration' })
   @IsNumber()
-  service_duration: number;
+  serviceDuration: number;
 
-  @ApiProperty({ description: '달력의 일/주/월/년 과 같이 용도별 표시' })
+  @ApiProperty({
+    description: '달력의 일/주/월/년 과 같이 용도별 표시',
+    default: 3,
+  })
   @IsOptional()
   @IsNumber()
   days?: number = 1;
 
   @ApiProperty({
+    name: 'timeslot_interval',
     description: '예약 가능한 시간대 간격 ( 예: 14:00, 14:30 )',
+    default: 1800,
   })
+  @Expose({ name: 'timeslot_interval' })
   @IsOptional()
   @IsNumber()
-  timeslot_interval?: number = 30;
+  timeslotInterval?: number = 30;
 
-  @ApiProperty({ description: 'true 인 경우, 다른 고객의 예약 고려 X' })
+  @ApiProperty({
+    name: 'is_ignore_schedule',
+    description: 'true 인 경우, 다른 고객의 예약 고려 X',
+    default: false,
+  })
+  @Expose({ name: 'is_ignore_schedule' })
   @IsOptional()
   @IsBoolean()
-  is_ignore_schedule?: boolean = false;
+  isIgnoreSchedule?: boolean = false;
 
-  @ApiProperty({ description: 'true 인 경우, 휴일,오픈시간,마감시간 고려 X' })
+  @ApiProperty({
+    name: 'is_ignore_workhour',
+    description: 'true 인 경우, 휴일,오픈시간,마감시간 고려 X',
+    default: false,
+  })
+  @Expose({ name: 'is_ignore_workhour' })
   @IsOptional()
   @IsBoolean()
-  is_ignore_workhour?: boolean = false;
+  isIgnoreWorkhour?: boolean = false;
 }
